c2295b5469106f63c4dfffa718521c28fa872a80
zhuangchaoxi <zhuangchaoxi@lamahui.com>
2017-04-17 16:09:15 +0800

    Update BaseCommands.py

--- BaseCommands.py
+++ BaseCommands.py
@@ -79,29 +79,4 @@ class BaseCommands(object):
 				break
 
 
-	def command_run(self, command, timeout):
-		result = self.SubCommands(command, timeout)
-		for rs in result:
-			key = rs.keys()[0]
-			value = rs[key]
-			if key == 'logging':
-				print "logging: ", value
-			elif key == 'execoutput':
-				print "execoutput: ", value
-			elif key == 'Last':
-				print "Last: ", value
-			elif key == 'return':
-				print "return: ", value
-			
-
-
-
-
-def main():
-	exeobj = BaseCommands()
-	exeobj.command_run("echo -e 'aaa\nbbb\n' && for i in {1..10};do echo $i ;done;echo abcd ; echo haha 1 2 3", 15)
-
-
-if __name__ == '__main__':
-	main()
 
